b29418ae167801d6f475cd5733c57f5f3302ffc4
cukmekerb <cukmekerb@gmail.com>
2021-07-09 12:09:40 -0700

    basic hello world

---
+++ gitdiff.lua
@@ -0,0 +1,10 @@
+local gitdiff = {}
+
+
+function gitdiff.changed_lines(diff)
+		local lines = {}
+		print("hello world")
+		return lines
+end
+
+return gitdiff
---
+++ test.lua
@@ -0,0 +1,4 @@
+-- this file tests the script
+require "gitdiff"
+
+print(io.read("*a"))
