55746ec007019e9b1a90adbf40ce349548dd5803
cukmekerb <cukmekerb@gmail.com>
2021-07-09 15:05:54 -0700

    hunks work and stuff

--- gitdiff.lua
+++ gitdiff.lua
@@ -1,13 +1,46 @@
 local gitdiff = {}
 
+-- liquidev is a genius
+local function extract_hunks(input)
+	local hunks = {}
+	local current_hunk = {}
+
+	local function end_hunk(new_line)
+		if #current_hunk > 0 then
+			table.insert(hunks, table.concat(current_hunk))
+			current_hunk = {new_line}
+		end
+	end
+
+	for line in input:gmatch("(.-)\n") do
+		if line:match("^@") then
+			end_hunk(line)
+		else
+			table.insert(current_hunk, line)
+		end
+	end
+
+	-- add the last hunk to the table
+	end_hunk("")
+
+	return hunks
+end
+
 -- this will only work on single-file diffs
 function gitdiff.changed_lines(diff)
-	local first_hunk_start = diff:match("@@%s+-%d+,%d+%s++(%d-),%d+%s+@@")
-	-- we are only interested in the stuff after the hunk
-	diff = diff:match("@@%s+-%d+,%d+%s++%d+,%d+%s+@@(.*)")
-	
-	print(first_hunk_start)
 	local changed_lines = {}
+	local hunks = extract_hunks(diff)
+	-- iterate over hunks
+	for i, hunk in pairs(hunks) do
+		local hunk_start = hunk:match("@@%s+-%d+,%d+%s++(%d-),%d+%s+@@")
+		if  hunk_start == nil then
+			print(hunk)
+			goto continue
+		end
+		print()
+		::continue::
+	end
+	
 	return changed_lines
 end
 
