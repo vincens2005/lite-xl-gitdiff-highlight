ac7059b8d1c5e4701253fe0dfaaf551ec7c5751a
cukmekerb <cukmekerb@gmail.com>
2021-07-09 19:40:37 -0700

    better tests

--- test.lua
+++ test.lua
@@ -3,10 +3,11 @@ local gitdiff = require "gitdiff"
 
 local input = io.read("*a")
 local lines = gitdiff.changed_lines(input)
+print("testing first line...")
 print(lines[1].line_number)
 print(lines[1].change_type)
-
+print()
+print("testing all lines...")
 for i, line in pairs(lines) do
-	print(line.change_type)
-	print(line.line_number)
+	print("line ".. line.line_number .. " - " .. line.change_type)
 end
