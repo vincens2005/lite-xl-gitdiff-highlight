259b217325198c223719069ad41f80585bcb4b3e
cukmekerb <cukmekerb@gmail.com>
2021-07-23 09:24:31 -0700

    working with test diff

--- init.lua
+++ init.lua
@@ -5,23 +5,33 @@ local common = require "core.common"
 local style = require "core.style"
 local gitdiff = require "plugins.gitdiff_highlight.gitdiff"
 
-style.gitdiff_addition = {common.color "#587c0c"} -- vscode default
+-- vscode defaults
+style.gitdiff_addition = {common.color "#587c0c"}
+style.gitdiff_modification = {common.color "#0c7d9d"}
+style.gitdiff_deletion = {common.color "#94151b"}
+
 style.gitdiff_width = 3
 
 -- test diff
 local current_diff = {
-	{
-		line_number = 12,
-		change_type = "addition"
-	},
-	{
-		line_number = 14,
-		change_type = "modification"
-	},
-	{
-		line_number = 20,
-		change_type = "deletion"
-	}
+	nil,
+	nil,
+	"addition",
+	"addition",
+	nil,
+	nil,
+	"modification",
+	"deletion",
+	nil,
+	nil,
+	"addition",
+	"modification",
+	"deletion",
+	"deletion",
+	"addition",
+	"modification",
+	"modification",
+	"modification"
 }
 
 local function gitdiff_padding(dv)
@@ -32,11 +42,27 @@ local old_docview_gutter = DocView.draw_line_gutter
 function DocView:draw_line_gutter(idx, x, y)
 	old_docview_gutter(self, idx, x, y)
 
+	if current_diff[idx] == nil then
+		return
+	end
+
+	local color = nil
+
+	if current_diff[idx] == "addition" then
+		color = style.gitdiff_addition
+
+	elseif current_diff[idx] == "modification" then
+		color = style.gitdiff_modification
+
+	else
+		color = style.gitdiff_deletion
+	end
+
 	-- add margin in between highlight and text
 	x = x + gitdiff_padding(self)
 
 	local yoffset = self:get_line_text_y_offset()
-	renderer.draw_rect(x, y + yoffset, style.gitdiff_width, self:get_line_height(), style.gitdiff_addition)
+	renderer.draw_rect(x, y + yoffset, style.gitdiff_width, self:get_line_height(), color)
 end
 
 local old_gutter_width = DocView.get_gutter_width
