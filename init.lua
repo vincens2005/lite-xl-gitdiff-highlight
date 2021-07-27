b38d450749ffa4c09fb9a78a3e289f26cfc9c737
cukmekerb <cukmekerb@gmail.com>
2021-07-26 21:04:40 -0700

    detect tab switching

--- init.lua
+++ init.lua
@@ -1,5 +1,6 @@
 -- mod-version:1
-
+local core = require "core"
+local config = require "core.config"
 local DocView = require "core.docview"
 local Doc = require "core.doc"
 local common = require "core.common"
@@ -35,6 +36,7 @@ local current_diff = {
 	"modification",
 	"modification"
 }
+local current_doc_name = nil
 
 local function gitdiff_padding(dv)
 	return style.padding.x * 1.5 + dv:get_font():get_width(#dv.doc.lines)
@@ -75,7 +77,7 @@ function Doc:on_text_change(type)
 	local line, col = self:get_selection()
 	if current_diff[line] == "addition" then goto end_of_function end
 	-- TODO figure out how to detect an addition
-	if type == "insert" or (type == "remove" and #self.lines == last_doc_lines) then	
+	if type == "insert" or (type == "remove" and #self.lines == last_doc_lines) then
 		current_diff[line] = "modification"
 	elseif type == "remove" then
 		current_diff[line] = "deletion"
@@ -83,4 +85,15 @@ function Doc:on_text_change(type)
 	::end_of_function::
 	last_doc_lines = #self.lines
 	return old_text_change(self, type)
+end
+
+local old_docview_update = DocView.update
+function DocView:update()
+	if current_doc_name ~= self:get_filename() and self:get_filename() ~= "---" and core.active_view.doc == self.doc then
+		-- TODO reload diff etc
+		core.log(self:get_filename())
+		current_doc_name = self:get_filename()
+	end
+
+	return old_docview_update(self)
 end
