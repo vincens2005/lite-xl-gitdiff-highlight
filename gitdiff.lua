6381b11a923c7949b842d9c2e2a08b8bf52c1520
Luke aka SwissalpS <Luke@SwissalpS.ws>
2023-07-08 20:47:22 +0200

    Update gitdiff.lua use ipairs on lists/arrays
    and one little whitespace style change

--- gitdiff.lua
+++ gitdiff.lua
@@ -9,7 +9,7 @@ local function extract_hunks(input)
 	local function end_hunk(new_line)
 		if #current_hunk > 0 then
 			table.insert(hunks, current_hunk)
-			current_hunk = {new_line}
+			current_hunk = { new_line }
 		end
 	end
 
@@ -33,7 +33,7 @@ function gitdiff.changed_lines(diff)
 	local changed_lines = {}
 	local hunks = extract_hunks(diff)
 	-- iterate over hunks
-	for _, hunk in pairs(hunks) do
+	for _, hunk in ipairs(hunks) do
 		local current_line
 		local hunk_start = hunk[1]:match("@@%s+-%d+,%d+%s++(%d-),%d+%s+@@")
 		hunk_start = tonumber(hunk_start)
@@ -46,7 +46,7 @@ function gitdiff.changed_lines(diff)
 		-- remove hunk header
 		hunk[1] = hunk[1]:gsub("@@%s+-%d+,%d+%s++%d+,%d+%s+@@", "")
 
-		for _, line in pairs(hunk) do
+		for _, line in ipairs(hunk) do
 			if line:match("^%s-%[%-.-%-]$") then
 				table.insert(changed_lines, {
 					line_number = current_line,
@@ -76,7 +76,7 @@ function gitdiff.changed_lines(diff)
 	end
 
 	local indexed_changed_lines = {}
-	for _, line in pairs(changed_lines) do
+	for _, line in ipairs(changed_lines) do
 		indexed_changed_lines[line.line_number] = line.change_type
 	end
 
